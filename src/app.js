20a8789a9c1d943e6e632516cf5733d70e16d191
jlopes <joao.victor@lojainterativa.com>
2025-04-29 21:04:40 -0300

    🚀 Adiciona suporte ao CORS, exemplo de arquivo .env e configurações relacionadas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const usersRoutes = require('./users/users.routes');
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/users', usersRoutes);
