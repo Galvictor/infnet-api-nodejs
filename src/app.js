60fea0f50e621a648e7af9cb6af9bbd6c310b28b
jlopes <joao.victor@lojainterativa.com>
2025-04-29 21:21:04 -0300

    🚀 Adicionado middleware de rate limit para limitar requisições e dependência `express-rate-limit`.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const {rateLimit} = require('express-rate-limit');
 const app = express();
 const usersRoutes = require('./users/users.routes');
 
-app.use(cors());
+// Configuração do rate limiter
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    limit: 100,
+    message: {
+        success: false,
+        message: 'Muitas requisições, tente novamente mais tarde.'
+    },
+    statusCode: 429
+});
 
+// Aplicar o rate limiter a todas as rotas
+app.use(limiter);
+
+app.use(cors());
 app.use(express.json());
 app.use('/users', usersRoutes);
 
